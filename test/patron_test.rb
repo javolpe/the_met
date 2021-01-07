15276ff513e905aef5653f972b6b7b85f59e2bb9
Jacob Volpe <volpe.jacob@gmail.com>
2021-01-07 09:15:20 -0700

    Finish patron test suite

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -12,5 +12,14 @@ class PatronTest < MiniTest::Test
     assert_instance_of Patron, patron_1
     assert_equal "Bob", patron_1.name
     assert_equal 20, patron_1.spending_money
+    assert_equal [], patron_1.interests
+  end
+
+  def test_patron_can_add_interst
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], patron_1.interests
   end
 end
