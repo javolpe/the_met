f34cf92532817986cba7f179a7f18a050e5c767c
Jacob Volpe <volpe.jacob@gmail.com>
2021-01-07 09:46:13 -0700

    Create musuem class, all tests in green

---
+++ lib/museum.rb
@@ -0,0 +1,25 @@
+class Museum
+  attr_reader :name,
+              :exhibits
+
+  def initialize(name)
+     @name = name
+     @exhibits = []
+  end
+
+  def add_exhibit(exhibit)
+    exhibits << exhibit
+  end
+
+  def recommend_exhibits(patron)
+    recommended = []
+    patron.interests.map do |interest|
+      @exhibits.map do |m_exhibit|
+        if m_exhibit.name == interest
+          recommended << m_exhibit
+        end
+      end
+    end
+    recommended
+  end
+end
