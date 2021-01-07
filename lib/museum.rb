d39c65f5566834831195e2961fc501ab263a2752
Jacob Volpe <volpe.jacob@gmail.com>
2021-01-07 09:52:54 -0700

    Write patrons admitted tests

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,12 @@
 class Museum
   attr_reader :name,
-              :exhibits
+              :exhibits,
+              :patrons
 
   def initialize(name)
      @name = name
      @exhibits = []
+     @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -22,4 +24,6 @@ class Museum
     end
     recommended
   end
+
+
 end
--- test/musuem_test.rb
+++ test/musuem_test.rb
@@ -12,6 +12,7 @@ class MusuemTest < MiniTest::Test
     assert_instance_of Museum, dmns
     assert_equal "Denver Museum of Nature and Science", dmns.name
     assert_equal [], dmns.exhibits
+    assert_equal [], dmns.patrons
   end
 
   def test_museum_can_add_exhibits
@@ -44,5 +45,27 @@ class MusuemTest < MiniTest::Test
 
     assert_equal [dead_sea_scrolls, gems_and_minerals], expected_patron_1
     assert_equal [imax], expected_patron_2
+  end
+
+  def test_patrons_admitted
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    assert_equal [patron_1, patron_3, patron_3], dmns.patrons
   end
 end
